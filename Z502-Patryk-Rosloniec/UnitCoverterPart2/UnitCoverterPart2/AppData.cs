0f19b5e50f57388d56a632632bc465a477640399
patryk-er <p.rosloniec@poczta.wwsi.edu.pl>
2019-11-09 13:12:39 +0100

    Patryk Rosłoniec - zadanie 2, commit początkowy

---
+++ Z502-Patryk-Rosloniec/UnitCoverterPart2/UnitCoverterPart2/AppData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitCoverterPart2
+{
+    static class AppData
+    {
+        public static string[] types = new string[] { "temperatura", "długość", "masa" };
+
+        public static string[] tUnits = new string[] { "°C", "°F", "K", "°R" };
+
+        public static string[] lUnits = new string[] { "mm", "cm", "dcm", "m", "km", "cali", "stóp", "jardów", "mil", "kabli", "mil morskich" };
+
+        public static double[] lMultiplier = new double[] { 1000, 100, 10, 1, 0.001, 39.3700787, 3.2808399, 1.093613, 0.000621371192, 0.0054, 0.0005399999568000035 };
+
+        public static string[] wUnits = new string[] { "mg", "g", "dkg", "kg", "T", "uncji", "funtów", "karatów", "kwintali" };
+
+        public static double[] wMultiplier = new double[] { 1000000, 1000, 100, 1, 0.001, 35.273962, 2.204623, 4999.999985, 0.01 };
+    }
+}
---
+++ Z502-Patryk-Rosloniec/UnitCoverterPart2/UnitCoverterPart2/StatisticsAzureStorageRepository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitCoverterPart2.Model;
+
+namespace UnitCoverterPart2
+{
+    public class StatisticsAzureStorageRepository: IStatisticsRepository
+    {
+        private CloudTable table;
+
+        public StatisticsAzureStorageRepository()
+        {
+            CloudStorageAccount storageAccount = new CloudStorageAccount(new StorageCredentials(ConfigurationManager.AppSettings["StorageName"], ConfigurationManager.AppSettings["StorageKey"]), true);
+            CloudTableClient client = storageAccount.CreateCloudTableClient();
+            this.table = client.GetTableReference("StatisticsMarcinIwanowski");
+            this.table.CreateIfNotExists();
+        }
+
+        public void AddStatistic(StatisticDTO statistic)
+        {
+            StatisticsEntity entity = new StatisticsEntity();
+            entity.PartitionKey = string.Empty; // computer name;
+            entity.RowKey = Guid.NewGuid().ToString();
+            entity.Type = statistic.Type;
+            entity.UnitTo = statistic.UnitTo;
+            entity.UnitFrom = statistic.UnitFrom;
+            entity.RawValue = statistic.RawValue;
+            entity.ConvertedValue = statistic.ConvertedValue;
+            entity.DateTime = statistic.DateTime;
+
+            TableOperation insertOperation = TableOperation.Insert(entity);
+
+            table.Execute(insertOperation);
+        }
+
+        public IEnumerable<StatisticDTO> GetStatistics()
+        {
+            TableQuery<StatisticsEntity> query = new TableQuery<StatisticsEntity>();
+
+            return table.ExecuteQuery(query).Select(obj => new StatisticDTO() { DateTime = obj.DateTime, Type = obj.Type, ConvertedValue = obj.ConvertedValue, RawValue = obj.RawValue, UnitFrom = obj.UnitFrom, UnitTo = obj.UnitTo }).ToList();
+        }
+    }
+}
