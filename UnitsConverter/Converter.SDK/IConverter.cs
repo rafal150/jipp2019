399772bd32145189c717758ee0f78500cdd443de
Maciej Pietrzak <mpietrzak@poczta.wwsi.edu.pl>
2019-11-21 16:49:39 +0100

    Commit 1, moduł 3 - utworzenie biblioteki klas.

---
+++ UnitsConverter/Converter.SDK/IConverter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Units_Converter
+{
+    public interface IConverter
+    {
+        string Name { get; }
+        List<string> Units { get; }
+        decimal Convert(string unitFrom, string unitTo, decimal value);
+    }
+}
