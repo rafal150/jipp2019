644a6eed883a7769f1fd6682b274efdd82991e3b
Łukasz Chudzik <lukaszchudzik96@gmail.com>
2019-11-23 01:48:18 +0100

    Dodano katalog bibliotek i pierwsza biblioteke DLL.

---
+++ YourUnits/YourUnitsMass.xaml.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace YourUnits
+{
+    /// <summary>
+    /// Logika interakcji dla klasy YourUnitsMass.xaml
+    /// </summary>
+    public partial class YourUnitsMass : Window
+    {
+        public YourUnitsMass()
+        {
+            InitializeComponent();
+        }
+
+        double TakeMiligram,    ShowMiligram;
+        double TakeGram,        ShowGram;
+        double TakeDecagram,    ShowDecagram;
+        double TakeKilogram,    ShowKilogram;
+        double TakeTonne,       ShowTonne;
+        double TakeOunce,       ShowOunce;
+        double TakePound,       ShowPound;
+        double TakeCarat,       ShowCarat;
+        double TakeQuintal,     ShowQuintal;
+
+        private void YUMT2_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            // Bug - martwy kod //
+        }
+
+        
+
+        private void YUMT1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                TakeMiligram = Convert.ToDouble(YUMT1.Text);
+
+                ShowGram = TakeMiligram / 1000;
+                YUMT2.Text = ShowGram.ToString();
+
+                ShowDecagram = TakeMiligram / 10000;
+                YUMT3.Text = ShowDecagram.ToString();
+
+                ShowKilogram = TakeMiligram / 1000000;
+                YUMT4.Text = ShowKilogram.ToString();
+
+                ShowTonne = TakeTonne / 1000000000;
+                YUMT5.Text = ShowTonne.ToString();
+            }
+        }
+
+        private void YUMT2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                TakeGram = Convert.ToDouble(YUMT2.Text);
+
+                ShowMiligram = TakeGram * 1000;
+                YUMT1.Text = ShowMiligram.ToString();
+
+                ShowDecagram = TakeGram / 10;
+                YUMT3.Text = ShowDecagram.ToString();
+
+                ShowKilogram = TakeGram / 1000;
+                YUMT4.Text = ShowKilogram.ToString();
+
+                ShowTonne = TakeGram / 1000000;
+                YUMT5.Text = ShowTonne.ToString();
+            }
+        }
+
+        private void YUMT3_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                TakeDecagram = Convert.ToDouble(YUMT3.Text);
+
+                ShowMiligram = TakeDecagram * 10000;
+                YUMT1.Text = ShowMiligram.ToString();
+
+                ShowGram = TakeDecagram * 10;
+                YUMT2.Text = ShowGram.ToString();
+
+                ShowKilogram = TakeDecagram / 100;
+                YUMT4.Text = ShowKilogram.ToString();
+
+                ShowTonne = TakeDecagram / 100000;
+                YUMT5.Text = ShowTonne.ToString();
+            }
+        }
+
+        private void YUMT4_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                TakeKilogram = Convert.ToDouble(YUMT4.Text);
+
+                ShowMiligram = TakeKilogram * 1000000;
+                YUMT1.Text = ShowMiligram.ToString();
+
+                ShowGram = TakeKilogram * 1000;
+                YUMT2.Text = ShowGram.ToString();
+
+                ShowDecagram = TakeKilogram * 100;
+                YUMT3.Text = ShowDecagram.ToString();
+
+                ShowTonne = TakeKilogram / 1000;
+                YUMT5.Text = ShowTonne.ToString();
+            }
+        }
+
+        private void YUMT5_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                TakeTonne = Convert.ToDouble(YUMT5.Text);
+
+                ShowMiligram = TakeTonne * 1000000000;
+                YUMT1.Text = ShowMiligram.ToString();
+
+                ShowGram = TakeTonne * 1000000;
+                YUMT2.Text = ShowGram.ToString();
+
+                ShowDecagram = TakeTonne * 100000;
+                YUMT3.Text = ShowDecagram.ToString();
+
+                ShowKilogram = TakeTonne * 1000;
+                YUMT4.Text = ShowKilogram.ToString();
+            }
+        }
+
+        private void YUMT6_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                TakeOunce = Convert.ToDouble(YUMT6.Text);
+
+                ShowPound = TakeOunce / 16;
+                YUMT7.Text = ShowPound.ToString();
+
+            }
+        }
+
+        private void YUMT7_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                TakePound = Convert.ToDouble(YUMT7.Text);
+
+                ShowOunce = TakePound * 16;
+                YUMT6.Text = ShowOunce.ToString();
+
+            }
+        }
+
+        private void YUMT8_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                TakeCarat = Convert.ToDouble(YUMT8.Text);
+
+                ShowGram = TakeCarat * 200;
+                YUMT2.Text = ShowGram.ToString();
+
+                ShowOunce = TakeCarat * 0.00705;
+                YUMT6.Text = ShowOunce.ToString();
+
+            }
+        }
+
+        private void YUMT9_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                TakeQuintal = Convert.ToDouble(YUMT9.Text);
+
+                ShowKilogram = TakeQuintal * 100;
+                YUMT4.Text = ShowKilogram.ToString();
+
+                ShowPound = TakeQuintal * 100;
+                YUMT7.Text = ShowPound.ToString();
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private void YUMT6_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void YUMT7_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+
+
+    }
+}
---
+++ YourUnits/YourUnitsTemperature.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace YourUnits
+{
+    /// <summary>
+    /// Logika interakcji dla klasy YourUnitsTemperature.xaml
+    /// </summary>
+    public partial class YourUnitsTemperature : Window
+    {
+        public YourUnitsTemperature()
+        {
+            InitializeComponent();
+        }
+            double ShowCelcius;
+            double TakeCelcius;
+            double TakeFarenheit;
+            double ShowFarenheit;
+            double TakeKelvin;
+            double ShowKelvin;
+            double ShowRankine;
+            double TakeRankine;
+
+        private void YUTT1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                TakeCelcius = Convert.ToDouble(YUTT1.Text);
+
+                ShowFarenheit = 2 * (TakeCelcius - 0.1 * TakeCelcius) + 32;
+                YUTT2.Text = ShowFarenheit.ToString();
+
+                ShowKelvin = TakeCelcius + 273.15;
+                YUTT3.Text = ShowKelvin.ToString();
+
+                ShowRankine = ShowKelvin *1.8;
+                YUTT4.Text = ShowRankine.ToString();
+            }
+        }
+
+        private void YUTT2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                TakeFarenheit = Convert.ToDouble(YUTT2.Text);
+
+                ShowCelcius = ((TakeFarenheit-32)/2) * 1.1;
+                YUTT1.Text = ShowCelcius.ToString();
+
+                ShowKelvin = ShowCelcius + 273.15;
+                YUTT3.Text = ShowKelvin.ToString();
+
+                ShowRankine = ShowKelvin * 1.8;
+                YUTT4.Text = ShowRankine.ToString();
+            }
+        }
+
+        private void YUTT3_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                TakeKelvin = Convert.ToDouble(YUTT3.Text);
+
+                ShowCelcius = TakeKelvin - 273.15;
+                YUTT1.Text = ShowCelcius.ToString();
+
+                ShowFarenheit = 2 * (ShowCelcius - 0.1 * ShowCelcius) + 32;
+                YUTT2.Text = ShowFarenheit.ToString();
+
+                ShowRankine = TakeKelvin * 1.8;
+                YUTT4.Text = ShowRankine.ToString();
+            }
+        }
+
+        private void YUTT4_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter )
+            {
+                TakeRankine = Convert.ToDouble(YUTT4.Text);
+
+                ShowKelvin = TakeRankine / 1.8;
+                YUTT3.Text = ShowKelvin.ToString();
+
+                ShowCelcius = ShowKelvin - 273.15;
+                YUTT1.Text = ShowCelcius.ToString();
+
+                ShowFarenheit = 2 * (ShowCelcius - 0.1 * ShowCelcius) + 32;
+                YUTT2.Text = ShowFarenheit.ToString();
+
+            }
+        }
+    }
+}
