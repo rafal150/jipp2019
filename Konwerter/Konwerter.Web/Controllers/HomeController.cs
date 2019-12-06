5313167e3f45671c171c0c1ac1ce74090e69bc88
Grzegorz Pluszczewicz <g_pluszczewicz@poczta.wwsi.edu.pl>
2019-12-06 15:03:31 +0100

    zadanie 4 - jeszcze nie działa

---
+++ Konwerter/Konwerter.Web/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using Autofac;
+using Konwerter.SDK;
+using Konwerter.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Konwerter.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ConvertersService convertersService;
+        private ILifetimeScope scope;
+
+        public HomeController(ILifetimeScope scope, IRepo repo, ConvertersService konwertery)
+        {
+            this.convertersService = konwertery;
+            this.scope = scope;
+        }
+        public ActionResult Index()
+        {
+            List<IKonwerter> konwertery = this.convertersService.GetConverters();
+
+            return View(konwertery);
+            //return View();
+        }
+
+        public double Przelicz(string fromType, string toType, string value, string converterType)
+        {
+            IKonwerter konwerter= this.scope.Resolve(Type.GetType(converterType)) as IKonwerter;
+
+            double wynik = konwerter.Przelicz(fromType, toType, double.Parse(value));
+
+            return wynik;
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
