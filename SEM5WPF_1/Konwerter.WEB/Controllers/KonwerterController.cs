7da846ae529e3d52ca52047ec25ebf9744a52e80
Rafał Szczęśniak <rszczesniak519@gmail.com>
2020-01-09 23:45:16 +0100

    Dodanie Api

---
+++ SEM5WPF_1/Konwerter.WEB/Controllers/KonwerterController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Autofac;
+using SEM5WPF_1.Services;
+
+namespace Konwerter.WEB.Controllers
+{
+    public class KonwerterController : ApiController
+    {
+        private KonwerterServices konwerterservices;
+        private ILifetimeScope scope;
+        public KonwerterController(ILifetimeScope scope, KonwerterServices konwerterServices)
+        {
+            this.konwerterservices = konwerterServices;
+            this.scope = scope;
+        }
+
+        public List<IKonwerter> GetKonwerters()
+        {
+            List<IKonwerter> konwerters = this.konwerterservices.GetKonwerters();
+
+            return konwerters;
+        }
+
+        [Route("api/konwerters/konwert")]
+        [HttpGet]
+        public decimal Konwerter(string jednostkaZ, string jednostkaDo, string wartosc,
+        string konwerterType)
+        {
+            IKonwerter konwerter = this.konwerterservices.GetKonwerters()
+                .Where(c => c.Name == konwerterType).FirstOrDefault();
+
+            decimal output = konwerter.Konwerter(jednostkaZ, jednostkaDo, decimal.Parse(wartosc));
+
+            return output;
+        }     
+    }
+}
---
+++ SEM5WPF_1/SEM5WPF_1/KonwertersApi.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections.Specialized;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Text;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace SEM5WPF_1
+{
+    class KonwertersApi
+    {
+
+        public List<Konwerter> GetConverters()
+        {
+            string url = @"https://localhost:44322/api/konwerters/konwert/";
+
+            using (WebClient client = new WebClient())
+            {
+                byte[] jsonData = client.DownloadData(url);
+                string json = Encoding.UTF8.GetString(jsonData);
+                Konwerter[] konwerters = JsonConvert.DeserializeObject<Konwerter[]>(json);
+                return new List<Konwerter>(konwerters);
+
+            }
+        }
+
+        public decimal Konwerter(string jednostkaZ, string jednostkaDo, string wartosc,
+       string konwerterType)
+        {
+            string url = @"https://localhost:44322/api/konwerters/konwert?";
+
+            NameValueCollection queryString = HttpUtility.ParseQueryString(string.Empty);
+            queryString.Add("jednostkaZ", jednostkaZ);
+            queryString.Add("jednostkaDo", jednostkaDo);
+            queryString.Add("wartosc", wartosc);
+            queryString.Add("konwerterType", konwerterType);
+
+            using (WebClient client = new WebClient())
+            {
+                string urlwithparameters = url + queryString.ToString();
+                string valueString = client.DownloadString(urlwithparameters);
+                return decimal.Parse(valueString);
+            }
+        }
+
+    }
+    public class Konwerter
+    {
+        public string Name { get; set; }
+        public List<string> Units { get; set; }
+    }
+}
