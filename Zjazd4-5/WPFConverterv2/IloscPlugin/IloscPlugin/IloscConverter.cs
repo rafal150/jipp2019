560ab8b0f845abc69e42d3fa5e5c933e55239abe
Filip Ścibor <kamaro7322@gmail.com>
2020-01-09 22:52:23 +0100

    Publikuje wykonany projekt na zjazd 4 w postaci webowej niestety z opóźnieniem z powodu spraw osobistych. Przepraszam

---
+++ Zjazd4-5/WPFConverterv2/IloscPlugin/IloscPlugin/IloscConverter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitCoverterPart2.Services;
+
+namespace IloscPlugin
+{
+    class IloscConverter : IConverter
+    {
+        public string Name => "Ilości";
+
+        public List<string> Units => new List<string>(new[] { "sztuka", "tuzin", "mendel" });
+
+        public double Convert(string unitFrom, string unitTo, double value)
+        {
+            if (unitFrom == "sztuka" && unitTo == "tuzin")        //kg na tone
+            {
+                value = value / 12;
+
+            }
+            if (unitFrom == "sztuka" && unitTo == "mendel")        //kg na tone
+            {
+                value = value / 15;
+
+            }
+            if (unitFrom == "tuzin" && unitTo == "sztuka")        //kg na tone
+            {
+                value = value * 12;
+
+            }
+            if (unitFrom == "tuzin" && unitTo == "mendel")        //kg na tone
+            {
+                value = value * 8 / 10;
+
+            }
+            if (unitFrom == "mendel" && unitTo == "sztuka")        //kg na tone
+            {
+                value = value * 15;
+
+            }
+            if (unitFrom == "mendel" && unitTo == "tuzin")        //kg na tone
+            {
+                value = value * 5 / 4;
+
+            }
+            return value;
+        }
+        }
+
+}
