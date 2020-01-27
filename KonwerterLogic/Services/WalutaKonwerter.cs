83bbcc6d694db51869a220f449d2622ea1744c26
JAKMIC <jakub.michalik25@gmail.com>
2020-01-27 23:15:06 +0100

    Jakub Michalik zadanie 5

---
+++ KonwerterLogic/Services/WalutaKonwerter.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace Konwerter.Services
+{
+    public class WalutaKonwerter : IKonwerter
+    {
+        public string Name => "Waluta";
+
+        public List<string> Units => new List<string>(new[] { "PLN", "EUR" });
+
+
+        public double Convert(string unitFrom, string unitTo, double value)
+        {
+            string url = @"http://api.nbp.pl/api/exchangerates/tables/a/?format=json";
+            double resultConvert = 0;
+            float eur = 0;
+
+            using (WebClient client = new WebClient())
+            {
+                string json = client.DownloadString(url);
+                TableObject[] tables = JsonConvert.DeserializeObject<TableObject[]>(json);
+
+                if (tables.Length > 0)
+                {
+                    RateObject rate = tables[0].Rates.Where(r => r.Code == "EUR").FirstOrDefault();
+
+                    if (rate != null)
+                    {
+                        eur = (rate.Mid);
+                    }
+                }
+
+            }
+
+            if(unitFrom == "PLN" && unitTo == "PLN")
+            {
+                resultConvert = value;
+            }
+            else if (unitFrom == "EUR" && unitTo == "EUR")
+            {
+                resultConvert = value;
+            }
+            else if (unitFrom == "PLN" && unitTo == "EUR")
+            {
+                resultConvert = value / eur;
+            }
+            else if (unitFrom == "EUR" && unitTo == "PLN")
+            {
+                resultConvert = value * eur;
+            }
+
+            return resultConvert;
+        }
+
+
+
+    }
+
+    public class TableObject
+    {
+        public string Table { get; set; }
+        public DateTime? EffectiveDate { get; set; }
+        public RateObject[] Rates { get; set; }
+    }
+
+    public class RateObject
+    {
+        public string Currency { get; set; }
+        public string Code { get; set; }
+        public float Mid { get; set; }
+    }
+
+}
