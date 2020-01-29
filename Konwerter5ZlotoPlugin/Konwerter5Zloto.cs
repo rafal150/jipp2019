4681ea116afa25e81d1664078b713bc0555fcb7b
Jan Wróblewski <jan.wroblewski@hotmail.com>
2020-01-29 22:15:12 +0100

    Konwerter zlotoPLN

---
+++ Konwerter5ZlotoPlugin/Konwerter5Zloto.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Konwerter5.Uslugi;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace Konwerter5ZlotoPlugin
+{
+    public class Konwerter5Zloto  : IKonwerter5
+    {
+        double wartoscDoPrzeliczen;
+        public string NazwaKategorii => "Złoto";
+        public List<string> Jednostki => new List<string>(new[] { "Złoto", "PLN" });
+
+
+        double SetPrzelicznik()
+        {
+            string NBPWebApiJson = @"http://api.nbp.pl/api/cenyzlota/?format=json";
+            using (WebClient webClient = new WebClient())
+            {
+                string JsonString = webClient.DownloadString(NBPWebApiJson);
+                var kurs = KursZlota.FromJson(JsonString);
+
+                return kurs[0].Cena;
+
+            }
+        }
+
+
+
+        public double Konwertuj(string zJednostki, string doJednostki, double wartosc)
+        {
+            Konwerter5Zloto konwersja = new Konwerter5Zloto
+            {
+                wartoscDoPrzeliczen = wartosc,
+            };
+            string ZJ = zJednostki;
+            string DJ = doJednostki;
+            if (ZJ == DJ)
+            {
+                return wartosc;
+            }
+            if (DJ == "PLN")
+            {
+                return wartoscDoPrzeliczen * SetPrzelicznik();
+            }
+            return wartoscDoPrzeliczen / SetPrzelicznik();
+
+        }
+    }
+}
---
+++ Konwerter5ZlotoPlugin/KursZlota.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using Newtonsoft.Json;
+
+namespace Konwerter5ZlotoPlugin
+{
+    class KursZlota
+    {
+        [JsonProperty("data")]
+        public DateTimeOffset Data { get; set; }
+
+        [JsonProperty("cena")]
+        public double Cena
+        {
+            get; set;
+        }
+
+        public static KursZlota[] FromJson(string json) => JsonConvert.DeserializeObject<KursZlota[]>(json, JsonConverter.Settings);
+    }
+}
+
+
