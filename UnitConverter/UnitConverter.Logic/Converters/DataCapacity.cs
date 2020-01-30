7a9b3ef3c5db269829cafdf0e9841c57baf72151
Michał Rosłaniec <m_roslaniec@poczta.wwsi.edu.pl>
2020-01-30 18:27:31 +0100

    == Zaliczenie - nowy konwerter == DataCapacity.cs - Gigabyte, Bluray

---
+++ UnitConverter/UnitConverter.Logic/Converters/DataCapacity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitConverter.Converters {
+    class DataCapacity : AbstractConverter, IConverter {
+        public List<string> Units => new List<string> {
+            "Gigabyte",
+            "Bluray",
+        };
+
+        public double ConvertGigabyteToBluray(double value) {
+            return value / 25;
+        }
+        public double ConvertBlurayToGigabyte(double value) {
+            return value * 25;
+        }
+    }
+}
