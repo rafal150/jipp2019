85bdc87ef341329863cd421dd7d10fb3586c58b9
JAKMIC <jakub.michalik25@gmail.com>
2020-01-30 18:28:15 +0100

    Dodanie konwertera ilości sztuka-kopa na zaliczenie

---
+++ KonwerterLogic/Services/IloscKonwerter.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konwerter.Services
+{
+    public class IloscKonwerter : IKonwerter
+    {
+        public string Name => "Ilość";
+
+        public List<string> Units => new List<string>(new[] { "Szt", "Kopa"});
+
+
+        public double Convert(string unitFrom, string unitTo, double value)
+        {
+            double toConvert = 0;
+            double resultConvert = 0;
+
+
+            //przekonwertowanie pierwotnej wartości
+            if (unitFrom == "Szt")
+            {
+                toConvert = value;
+            }
+            if (unitFrom == "Kopa")
+            {
+                toConvert = KopaToSzt(value);
+            }
+
+            if (unitTo == "Szt")
+            {
+                resultConvert = toConvert;
+            }
+            if (unitTo == "Kopa")
+            {
+                resultConvert = SztToKopa(toConvert);
+            }
+
+
+            return resultConvert;
+        }
+
+        public double SztToKopa(double ilosc)
+        {
+            ilosc = ilosc / 60;
+            return ilosc;
+        }
+        public double KopaToSzt(double ilosc)
+        {
+            ilosc = ilosc * 60;
+            return ilosc;
+        }
+
+    }
+
+}
