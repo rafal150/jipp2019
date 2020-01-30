775adde64def83f26cbf9e9c25bb8af35a6a23c8
Adam Nowakowski <nowakowskiadam123@gmail.com>
2020-01-30 17:01:03 +0100

    Adam Nowakowski -Plugin- zaliczenie 30.01

---
+++ Mocy_AN_Plugin/MocyANPlugin/MocyANPlugin.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnitCoverterPart2.Services;
+
+namespace MocyANPlugin
+{
+    class MocyANPlugin
+    {
+        class ANConverter : IConverter
+        {
+            public string Name => "KonwerterMocy";
+
+            public List<string> Units => new List<string>(new[] { "Watt", "Kilowat", "KonMechaniczny" });
+
+            public double Convert(string unitFrom, string unitTo, double value)
+            {
+                if (unitFrom == "Watt" && unitTo == "Kilowat")
+                {
+                    value = value * 0.001;
+
+                }
+                if (unitFrom == "Watt" && unitTo == "KonMechaniczny")
+                {
+                    value = value * 0.0014;
+
+                }
+                if (unitFrom == "Kilowat" && unitTo == "Watt")
+                {
+                    value = value * 1000;
+
+                }
+                if (unitFrom == "Kilowat" && unitTo == "KonMechaniczny")
+                {
+                    value = value * 1.3596;
+
+                }
+                if (unitFrom == "KonMechaniczny" && unitTo == "Watt")
+                {
+                    value = value * 735.4988;
+
+                }
+                if (unitFrom == "KonMechaniczny" && unitTo == "Kilowat")
+                {
+                    value = value * 0.7355;
+
+                }
+                return value;
+            }
+        }
+
+
+
+    }
+}
+
