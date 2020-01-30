bd39e2d3597ffadf56de483c0cc8124439a6c2d1
Michał Rosłaniec <m_roslaniec@poczta.wwsi.edu.pl>
2020-01-30 17:50:20 +0100

    UnitConverter.Logic: Length.cs - poprawki Volume.cs - poprawki

--- UnitConverter/UnitConverter.Logic/Converters/Length.cs
+++ UnitConverter/UnitConverter.Logic/Converters/Length.cs
@@ -7,8 +7,28 @@ using System.Threading.Tasks;
 namespace UnitConverter.Converters {
     class Length : AbstractConverter, IConverter {
         public List<string> Units => new List<string> {
-            "Meter",
-            "Kilometer",
+            "Metre",
+            "Kilometre",
+            "Lightyear",
         };
+
+        public double ConvertKilometreToMetre(double value) {
+            return value * 1000;
+        }
+        public double ConvertKilometreToLightyear(double value) {
+            return value / 9460700000000;
+        }
+        public double ConvertMetreToKilometre(double value) {
+            return value / 1000;
+        }
+        public double ConvertMetreToLightyear(double value) {
+            return ConvertKilometreToLightyear(ConvertMetreToKilometre(value));
+        }
+        public double ConvertLightyearToKilometre(double value) {
+            return value * 9460700000000;
+        }
+        public double ConvertLightyearToMetre(double value) {
+            return ConvertKilometreToMetre(ConvertLightyearToKilometre(value));
+        }
     }
 }
--- UnitConverter/UnitConverter.Logic/Converters/Volume.cs
+++ UnitConverter/UnitConverter.Logic/Converters/Volume.cs
@@ -8,7 +8,27 @@ namespace UnitConverter.Converters {
     class Volume : AbstractConverter, IConverter {
         public List<string> Units => new List<string> {
             "Litre",
+            "Decilitre",
             "Hectolitre",
         };
+
+        public double ConvertLitreToHectolitre(double value) {
+            return value / 100;
+        }
+        public double ConvertLitreToDecilitre(double value) {
+            return value / 10;
+        }
+        public double ConvertDecilitreToLitre(double value) {
+            return value * 10;
+        }
+        public double ConvertDecilitreToHectolitre(double value) {
+            return ConvertLitreToHectolitre(ConvertDecilitreToLitre(value));
+        }
+        public double ConvertHectolitreToLitre(double value) {
+            return value * 100;
+        }
+        public double ConvertHectolitreToDecilitre(double value) {
+            return ConvertLitreToDecilitre(ConvertHectolitreToLitre(value));
+        }
     }
 }
