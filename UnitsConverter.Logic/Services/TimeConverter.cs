a387c9b47e58b3f7fb9d263dbd64425363dfb12e
Bosial555 <60443914+Bosial555@users.noreply.github.com>
2020-01-31 14:43:31 +0100

    nowy konwerter

---
+++ UnitsConverter.Logic/Services/TimeConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitsConverter.Services
+{
+    class TimeConverter : IConverter
+    {
+        public string Name => "Czas";
+        public List<string> Units => new List<string>(new[] { "dzień", "tydzień"});
+        
+        
+        public decimal Convert(string unitFrom, string unitTo, decimal valueFrom)
+        {
+
+            if ( unitFrom == "dzień" && unitTo == "dzień")
+                return valueFrom;
+            else if (unitFrom == "dzień" && unitTo == "tydzień")
+                return valueFrom/7;
+            else if (unitFrom == "tydzień" && unitTo == "dzień")
+                return valueFrom * 7;
+            else if (unitFrom == "tydzień" && unitTo == "tydzień")
+                return valueFrom;
+
+            return 0;
+
+        }
+
+    }
+}
