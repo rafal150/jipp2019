281ddac07d8e373c34c95214ee3f7eeb933d679f
Damxx <damian.jakub.padzik@gmail.com>
2020-02-29 14:30:56 +0100

    Program na zaliczenie

---
+++ MOJ PROGRAM NA ZAL/UnitConverter.Logic/Services/TempConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KonwerterZSQLiAZUREiPLUGIN.Services
+{
+    public class TempConverter : IKonwerter
+    {
+        public string Name => "Czas";
+
+        public List<string> Units => new List<string>(new[] { "Dni", "Wiek(100lat)" });
+
+        public decimal Convert(string unitFrom, string unitTo, decimal value)
+        {
+
+            if (unitFrom == "Dni" && unitTo == "Wiek(100lat)")
+            {
+                value= value/(365 * 100);
+                return value;
+            }
+
+            else if (unitFrom == "Wiek(100lat)")
+            {
+                value = value * 36500;
+                return value;
+            }
+           
+
+            else
+                return value; 
+            }
+
+   
+        
+    }
+}
---
+++ MOJ PROGRAM NA ZAL/UnitConverter.Logic/StatystykiSQL.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KonwerterZSQLiAZUREiPLUGIN.Model;
+
+namespace KonwerterZSQLiAZUREiPLUGIN
+{
+    public class StatystykiSQL : RepozytoriumStatystyk
+    {
+        public void AddStatistic(PolaDataGrid statistic)
+        {
+            using (StatisticsModel context = new StatisticsModel())
+            {
+                context.Statistics.Add(new Statistic()
+                {
+                    Type = statistic.Type,
+                    DateTime = statistic.DateTime
+                });
+
+                context.SaveChanges();
+            }
+        }
+
+        public IEnumerable<PolaDataGrid> GetStatistics()
+        {
+            using (StatisticsModel context = new StatisticsModel())
+            {
+                return context.Statistics.
+                    Select(obj => new PolaDataGrid() { DateTime = obj.DateTime, Type = obj.Type }).
+                    ToList();
+            }
+        }
+    }
+}
