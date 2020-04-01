ee9531028e98cbe72481bedd61c39013d91efcc6
Rafał Marzec <rafalmarzecmarzecrafal@gmail.com>
2020-04-02 00:42:29 +0200

    zaliczenie na 3

---
+++ Konwerter.Logic/Statistics.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Konwerter
+{
+    public interface Statistics
+    {
+        void AddStatistic(DataGrid statistic);
+        IEnumerable<DataGrid> GetStatistics();
+    }
+}
